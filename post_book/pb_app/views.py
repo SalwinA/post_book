aa05f785030d75a41b329c33f97fd89da0e7803c
SalwinA <salwino@gmail.com>
2022-10-30 14:32:41 +0000

    Create model

--- post_book/pb_app/views.py
+++ post_book/pb_app/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+
+def signup(request):
+    return render(request, 'signup.html')
