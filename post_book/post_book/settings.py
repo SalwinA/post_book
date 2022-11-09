9a6325d269d585248b6df5d61b12bab863624039
SalwinA <salwino@gmail.com>
2022-11-09 18:13:07 +0000

    Final commit.

--- post_book/post_book/settings.py
+++ post_book/post_book/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%y#r*66+le-e&p&sio%_lu7zgo&-k*2r-b)0)+9&pp5ki77j&x'
+SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://git.heroku.com/post-book-22.git']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
