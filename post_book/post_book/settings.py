490df69c1f4ea42fcf959ce2b0ab688f9a795525
SalwinA <salwino@gmail.com>
2022-11-09 13:12:00 +0000

    Prepare to deploy.

--- post_book/post_book/settings.py
+++ post_book/post_book/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,13 +77,16 @@ WSGI_APPLICATION = 'post_book.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse('postgres://csjmgsjbrnvkkl:322db9279a712b2a759a78ed5d713456766182a4e9675b010a943f8d8a53566d@ec2-176-34-215-248.eu-west-1.compute.amazonaws.com:5432/d65ng5ml2vkdtc')
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
